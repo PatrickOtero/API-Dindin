1f070e4471be5bb9705d3cf10c6154d66d75d984
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-04-24 18:28:36 -0300

    Controlador de listagem aprimorado para retornar dados ordenados por data e por dia de semana, crescente e decrescente para ambos

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -15,16 +15,56 @@ const listTransactions = async (req, res) => {
   const { id } = req.user
   const allTransactions = await knex('transactions').where('user_id', id)
 
+  const dateDescendent = await knex('transactions').orderBy(
+    'registry_date',
+    'desc',
+  )
+  const dateAscendent = await knex('transactions').orderBy(
+    'registry_date',
+    'asc',
+  )
+
   const { totalIncoming, totalOutgoing, balance } = totalAmountObtainer(
     allTransactions,
   )
-
   dateFormatter(allTransactions)
   arrayPropertyValueFormatter(allTransactions)
 
-  return res
-    .status(200)
-    .json({ allTransactions, totalIncoming, totalOutgoing, balance })
+  dateFormatter(dateDescendent)
+  arrayPropertyValueFormatter(dateDescendent)
+
+  dateFormatter(dateAscendent)
+  arrayPropertyValueFormatter(dateAscendent)
+
+  const weekdays = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ]
+
+  const weekDescendent = await knex('transactions').orderBy('week_day', 'desc')
+  const weekAscendent = await knex('transactions').orderBy('week_day', 'asc')
+
+  dateFormatter(weekDescendent)
+  arrayPropertyValueFormatter(weekDescendent)
+
+  dateFormatter(weekAscendent)
+  arrayPropertyValueFormatter(weekAscendent)
+
+  return res.status(200).json({
+    allTransactions,
+    dateDescendent,
+    dateAscendent,
+    weekDescendent,
+    weekAscendent,
+    totalIncoming,
+    totalOutgoing,
+    balance,
+  })
 }
 
 const detailTransaction = async (req, res) => {
@@ -57,13 +97,13 @@ const createTransaction = async (req, res) => {
     await createTransactionSchema.validate(req.body)
 
     const weekdays = [
-      'Domingo',
-      'Segunda',
-      'Terça',
-      'Quarta',
-      'Quinta',
-      'Sexta',
-      'Sábado',
+      '1Domingo',
+      '2Segunda',
+      '3Terça',
+      '4Quarta',
+      '5Quinta',
+      '6Sexta',
+      '7Sábado',
     ]
     const date = new Date(registry_date)
     const weekday = date.getDay()
