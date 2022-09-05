769dee955c87569f1f9740c8568343aa8c433dae
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-09-04 21:47:28 -0300

    tentativa de novo deploy no Railway

--- src/connection.js
+++ src/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     user: process.env.DB_SERVER_USER || process.env.DB_LOCAL_USER,
-    port: DB_SERVER_PORT || 5432,
+    port: 6956 || 5432,
     host: process.env.DB_SERVER_HOST || process.env.DB_LOCAL_HOST,
     database: process.env.DB_SERVER_DATABASE || process.env.DB_LOCAL_DATABASE,
     password: process.env.DB_SERVER_PASSWORD || process.env.DB_LOCAL_PASSWORD,
